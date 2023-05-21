41ac8f05ba385895ac08d579354780434e405cca
G-Tech Solutions, Australia <79989480+salamalsam@users.noreply.github.com>
2023-05-21 09:39:46 +0500

    Add project files.

---
+++ connect.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$server_name = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'crud_app';
+
+$con_obj = new mysqli($server_name,$username,$password,$database);
+
+if($con_obj->connect_errno):
+	die('Connection Faild:'. $con_obj->connect_error);
+endif;
+
+echo 'Greetings 🙂';
+
+?>
